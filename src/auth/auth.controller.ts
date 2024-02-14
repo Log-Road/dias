d3652e72c273d68e25b8e86c061958f2a7fc498d
iixanx <iixiixanx@gmail.com>
2024-02-14 09:12:10 +0900

    init

---
+++ src/auth/auth.controller.ts
@@ -0,0 +1,11 @@
+import { Controller } from '@nestjs/common';
+import { AuthService } from './auth.service';
+
+@Controller('auth')
+export class AuthController {
+    constructor (
+        private authService: AuthService
+    ) {
+        this.authService = authService
+    }
+}
