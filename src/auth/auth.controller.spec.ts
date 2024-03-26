7eb60edc64e3efa26e500be3f7313f3baec5e169
iixanx <iixiixanx@gmail.com>
2024-03-26 16:22:50 +0900

    feat: refresh verify

--- src/auth/auth.controller.spec.ts
+++ src/auth/auth.controller.spec.ts
@@ -1,7 +1,12 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { AuthController } from './auth.controller';
 import { AuthService } from './auth.service';
-import { BadRequestException, Logger, NotFoundException } from '@nestjs/common';
+import {
+  BadRequestException,
+  InternalServerErrorException,
+  Logger,
+  NotFoundException,
+} from '@nestjs/common';
 import { PrismaService } from '../prisma/prisma.service';
 import { ConfigService } from '@nestjs/config';
 import { JwtService } from '@nestjs/jwt';
@@ -9,6 +14,8 @@ import { JwtService } from '@nestjs/jwt';
 describe('AuthController', () => {
   let controller: AuthController;
   let service: AuthService;
+  let jwt: JwtService;
+  let prisma: PrismaService;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
@@ -24,6 +31,8 @@ describe('AuthController', () => {
 
     controller = module.get<AuthController>(AuthController);
     service = module.get<AuthService>(AuthService);
+    jwt = module.get<JwtService>(JwtService);
+    prisma = module.get<PrismaService>(PrismaService);
   });
 
   describe('/signin', () => {
@@ -37,13 +46,13 @@ describe('AuthController', () => {
         accesstoken: 'testcodeaccesstoken',
         expiredAt: '2024-03-25T03:25:59.238Z',
         refreshtoken: 'testcoderefreshtoken',
-      }
+      };
 
       const response = {
         data,
         statusCode: 201,
         statusMsg: '로그인',
-      }
+      };
 
       jest.spyOn(service, 'signIn').mockImplementation(async () => await data);
 
@@ -76,6 +85,84 @@ describe('AuthController', () => {
             password: 'thisIsTest1!',
           }),
       ).rejects.toThrowError(new NotFoundException());
+    });
+  });
+
+  describe('regenerate refreshtoken', () => {
+    const accessToken =
+      'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6MTAsImlhdCI6MTcxMTMyNjM1OSwiZXhwIjoxNzExMzQwNzU5fQ.0UbgRd-ZxhNdAnFvVtatAiNpALsxEkf-vDTpy9zfNIQ';
+    const expiredAt = '2024-03-25T03:25:59.238Z';
+    const refreshToken =
+      'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6MTAsImlhdCI6MTcxMTMyNjM1OSwiZXhwIjoxNzEyNTM1OTU5fQ.AVfdnjbeZ-vErFwbxSMiT_lf7wZGKdMW72mo5GOAFHY';
+
+    it('[200] success', async () => {
+      const request =
+        'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6MTAsImlhdCI6MTcxMTM4MjA2OSwiZXhwIjoxNzEyNTkxNjY5fQ.8ggYTBXBtPqcfveXX4nVAHUJYzp0uaeyAKAoQxxqVUE';
+
+      const response = {
+        data: {
+          accessToken,
+          expiredAt,
+          refreshToken,
+        },
+        statusCode: 200,
+        statusMsg: '토큰 재생성',
+      };
+
+      jest.spyOn(service, 'verifyToken').mockImplementation(
+        async () =>
+          await {
+            accessToken,
+            expiredAt,
+            refreshToken,
+          },
+      );
+
+      expect(controller.verifyToken(request)).resolves.toStrictEqual(response);
+    });
+
+    it('[404] 존재하지 않는 사용자 아이디', async () => {
+      const request =
+        'Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6MTAsImlhdCI6MTcxMTM4MjA2OSwiZXhwIjoxNzEyNTkxNjY5fQ.8ggYTBXBtPqcfveXX4nVAHUJYzp0uaeyAKAoQxxqVUE';
+
+      jest.spyOn(jwt, 'verifyAsync').mockImplementation(
+        async () =>
+          await {
+            id: 0,
+            iat: 123879705,
+            exp: 12389174979,
+          },
+      );
+
+      jest.spyOn(prisma, 'findUserById').mockImplementation(null);
+
+      await expect(
+        async () => await controller.verifyToken(request),
+      ).rejects.toThrowError(new NotFoundException('존재하지 않는 유저'));
+    });
+
+    it('[500] Internal Server Error - Bearer Token 양식 위반', async () => {
+      const request =
+        'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6MTAsImlhdCI6MTcxMTM4MjA2OSwiZXhwIjoxNzEyNTkxNjY5fQ.8ggYTBXBtPqcfveXX4nVAHUJYzp0uaeyAKAoQxxqVUE';
+
+      await expect(
+        async () => await controller.verifyToken(request),
+      ).rejects.toThrowError(
+        new InternalServerErrorException('jwt must be provided'),
+      );
+    });
+
+    it('[500] Internal Server Error - Token 시간 만료', async () => {
+      const request =
+        'Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6MTAsImlhdCI6MTcxMTM4MjA2OSwiZXhwIjoxNzEyNTkxNjY5fQ.8ggYTBXBtPqcfveXX4nVAHUJYzp0uaeyAKAoQxxqVUE';
+
+      jest.spyOn(jwt, 'verifyAsync').mockImplementation(() => {
+        throw new InternalServerErrorException();
+      });
+
+      await expect(
+        async () => await controller.verifyToken(request),
+      ).rejects.toThrowError(new InternalServerErrorException());
     });
   });
 });
--- src/auth/auth.service.spec.ts
+++ src/auth/auth.service.spec.ts
@@ -1,7 +1,12 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { AuthService } from './auth.service';
 import { PrismaService } from '../prisma/prisma.service';
-import { BadRequestException, Logger, NotFoundException } from '@nestjs/common';
+import {
+  BadRequestException,
+  InternalServerErrorException,
+  Logger,
+  NotFoundException,
+} from '@nestjs/common';
 import { ConfigService } from '@nestjs/config';
 import { JwtService } from '@nestjs/jwt';
 import * as bcrypt from 'bcrypt';
@@ -9,6 +14,7 @@ import * as bcrypt from 'bcrypt';
 describe('AuthService', () => {
   let service: AuthService;
   let prisma: PrismaService;
+  let jwt: JwtService;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
@@ -23,6 +29,7 @@ describe('AuthService', () => {
 
     service = module.get<AuthService>(AuthService);
     prisma = module.get<PrismaService>(PrismaService);
+    jwt = module.get<JwtService>(JwtService);
 
     jest.spyOn(service, 'genAccessToken').mockImplementation(async () => ({
       accessToken:
@@ -90,6 +97,60 @@ describe('AuthService', () => {
       await expect(
         async () => await service.signIn(request),
       ).rejects.toThrowError(new NotFoundException());
+    });
+  });
+
+  describe('regenerate refreshtoken', () => {
+    it('[200] success', async () => {
+      const request =
+        'Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6MTAsImlhdCI6MTcxMTM4MjA2OSwiZXhwIjoxNzEyNTkxNjY5fQ.8ggYTBXBtPqcfveXX4nVAHUJYzp0uaeyAKAoQxxqVUE';
+
+      const response = {
+        accessToken:
+          'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6MTAsImlhdCI6MTcxMTMyNjM1OSwiZXhwIjoxNzExMzQwNzU5fQ.0UbgRd-ZxhNdAnFvVtatAiNpALsxEkf-vDTpy9zfNIQ',
+        expiredAt: '2024-03-25T03:25:59.238Z',
+        refreshToken:
+          'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6MTAsImlhdCI6MTcxMTMyNjM1OSwiZXhwIjoxNzEyNTM1OTU5fQ.AVfdnjbeZ-vErFwbxSMiT_lf7wZGKdMW72mo5GOAFHY',
+      };
+
+      await expect(service.verifyToken(request)).resolves.toStrictEqual(
+        response,
+      );
+    });
+
+    it('[404] 존재하지 않는 사용자 아이디', async () => {
+      const request =
+        'Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6MTAsImlhdCI6MTcxMTMyNjM1OSwiZXhwIjoxNzEyNTM1OTU5fQ.AVfdnjbeZ-vErFwbxSMiT_lf7wZGKdMW72mo5GOAFHY';
+
+      jest.spyOn(prisma, 'findUserById').mockImplementationOnce(null);
+
+      await expect(
+        async () => await service.verifyToken(request),
+      ).rejects.toThrowError(new NotFoundException('존재하지 않는 유저'));
+    });
+
+    it('[500] Internal Server Error - Bearer Token 양식 위반', async () => {
+      const request =
+        'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6MTAsImlhdCI6MTcxMTMyNjM1OSwiZXhwIjoxNzEyNTM1OTU5fQ.AVfdnjbeZ-vErFwbxSMiT_lf7wZGKdMW72mo5GOAFHY';
+
+      await expect(
+        async () => await service.verifyToken(request),
+      ).rejects.toThrowError(
+        new InternalServerErrorException('jwt must be provided'),
+      );
+    });
+
+    it('[500] Internal Server Error - Token 시간 만료', async () => {
+      const request =
+        'Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6MTAsImlhdCI6MTcxMTMyNjM1OSwiZXhwIjoxNzEyNTM1OTU5fQ.AVfdnjbeZ-vErFwbxSMiT_lf7wZGKdMW72mo5GOAFHY';
+
+      jest.spyOn(jwt, 'verifyAsync').mockImplementation(async () => {
+        throw new InternalServerErrorException();
+      });
+
+      await expect(
+        async () => await service.verifyToken(request),
+      ).rejects.toThrowError(new InternalServerErrorException());
     });
   });
 });
