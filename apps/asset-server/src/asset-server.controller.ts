ed70386deaff8ce768067a9ae1e600241a0ee18f
iixanx <iixiixanx@gmail.com>
2024-07-09 15:12:31 +0900

    fix: test code fixed

--- apps/asset-server/src/asset-server.controller.ts
+++ apps/asset-server/src/asset-server.controller.ts
@@ -2,9 +2,9 @@ import { Body, Controller, Get } from '@nestjs/common';
 import { AssetServerService } from './asset-server.service';
 import { UseGuards } from '@nestjs/common'
 import { JwtAuthGuard } from '../../dias/src/auth/strategies/jwt/jwt.auth.guard'
-import { ROLE } from 'apps/dias/src/utils/type.util';
-import { Role } from 'apps/dias/src/guard/role/role.decorator';
-import { RoleGuard } from 'apps/dias/src/guard/role/role.guard';
+import { ROLE } from '../../dias/src/utils/type.util';
+import { Role } from '../../dias/src/guard/role/role.decorator';
+import { RoleGuard } from '../../dias/src/guard/role/role.guard';
 
 @UseGuards(JwtAuthGuard, RoleGuard)
 @Controller()
