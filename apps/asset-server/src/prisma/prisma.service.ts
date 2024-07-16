c5b67b81e41dfc78dd4ba1c37a4487718a30a825
iixanx <iixiixanx@gmail.com>
2024-07-16 14:07:22 +0900

    fix: prisma client

--- apps/asset-server/src/prisma/prisma.service.ts
+++ apps/asset-server/src/prisma/prisma.service.ts
@@ -1,6 +1,6 @@
 import { Injectable, OnModuleDestroy, OnModuleInit } from "@nestjs/common";
 import { ConfigService } from "@nestjs/config";
-import { PrismaClient } from "@prisma/client";
+import { PrismaClient } from "./client";
 
 @Injectable()
 export class PrismaService
