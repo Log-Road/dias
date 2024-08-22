4b2982a6ae8ed2130ff2077fa4ad3cfda6bb4a0a
iixanx <iixiixanx@gmail.com>
2024-08-22 16:35:31 +0900

    fix: testing

--- apps/admin-server/src/competition/test/competition.controller.spec.ts
+++ apps/admin-server/src/competition/test/competition.controller.spec.ts
@@ -1,18 +1,33 @@
-import { Test, TestingModule } from '@nestjs/testing';
-import { CompetitionController } from '../competition.controller';
+import { Test, TestingModule } from "@nestjs/testing";
+import { CompetitionController } from "../competition.controller";
+import { JwtService } from "@nestjs/jwt";
+import { PrismaService as UserPrismaService } from "../../../../dias/src/prisma/prisma.service";
+import { PrismaService } from "../../prisma/prisma.service";
+import { CompetitionService } from "../competition.service";
+import { Logger } from "@nestjs/common";
 
-describe('CompetitionController', () => {
+describe("CompetitionController", () => {
   let controller: CompetitionController;
+
+  const prismaMock = {};
+  const userPrismaMock = {};
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
       controllers: [CompetitionController],
+      providers: [
+        CompetitionService,
+        { provide: PrismaService, useValue: prismaMock },
+        { provide: UserPrismaService, useValue: userPrismaMock },
+        JwtService,
+        Logger,
+      ],
     }).compile();
 
     controller = module.get<CompetitionController>(CompetitionController);
   });
 
-  it('should be defined', () => {
+  it("should be defined", () => {
     expect(controller).toBeDefined();
   });
 });
